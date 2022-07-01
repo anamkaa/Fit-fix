73ba631f0e5ca768210b39ceecb43111ed5a388c
anamkaa <anamikakashyap99@gmail.com>
2022-07-01 15:55:39 +0530

    added routing for pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
+import VideoListingPage from "./pages/VideoListingPage";
+import VideoPage from "./pages/VideoPage";
 
 function App() {
   return (
     <div className="App">
-      <LandingPage />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/videoListingPage" element={<VideoListingPage />} />
+        <Route path="/videoPage" element={<VideoPage />} />
+      </Routes>
     </div>
   );
 }
