f132e8c711ed7917d5a958da79e25d5defb011d9
anamkaa <anamikakashyap99@gmail.com>
2022-07-05 00:06:40 +0530

    updated database

--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -9,10 +9,19 @@ import { formatDate } from "../utils/authUtils";
 export const users = [
   {
     _id: uuid(),
-    firstName: "Adarsh",
-    lastName: "Balika",
-    email: "adarshbalika@gmail.com",
-    password: "adarshBalika123",
+    firstName: "Guest",
+    lastName: "User",
+    email: "guestuser@gmail.com",
+    password: "guestuser123",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+  {
+    _id: uuid(),
+    firstName: "Jacob",
+    lastName: "Ray",
+    email: "jacobray@gmail.com",
+    password: "jacobray123",
     createdAt: formatDate(),
     updatedAt: formatDate(),
   },
