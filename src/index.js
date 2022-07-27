32b93503cf2e8da4e01513084f22dd9d05f007fb
anamkaa <75376715+anamkaa@users.noreply.github.com>
2022-07-27 18:04:07 +0530

    imported video , like and watchlater provider

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import { makeServer } from "./server";
 import { BrowserRouter } from "react-router-dom";
 import { FilterProvider } from "../src/context/filter-context";
 import { AuthProvider } from "./context/auth-context";
+import { LikesProvider } from "./context/likes-context";
+import { VideoProvider } from "./context/video-context";
+import { WatchlaterProvider } from "./context/watchlater-context";
 
 // Call make Server
 makeServer();
@@ -13,11 +16,17 @@ makeServer();
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider>
-        <FilterProvider>
-          <App />
-        </FilterProvider>
-      </AuthProvider>
+      <VideoProvider>
+        <AuthProvider>
+          <LikesProvider>
+            <WatchlaterProvider>
+              <FilterProvider>
+                <App />
+              </FilterProvider>
+            </WatchlaterProvider>
+          </LikesProvider>
+        </AuthProvider>
+      </VideoProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
