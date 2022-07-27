b42386bd2679a0b7c94348e139719b9b1253d91a
anamkaa <75376715+anamkaa@users.noreply.github.com>
2022-07-27 18:03:08 +0530

    created like context , reducer and services

---
+++ src/reducer/reducer-like.js
@@ -0,0 +1,15 @@
+export const likesReducer = (state, action) => {
+  switch (action.type) {
+    case "GET_LIKES":
+      return { ...state, likes: action.payload };
+
+    case "ADD_TO_LIKES":
+      return { ...state, likes: action.payload };
+
+    case "REMOVE_FROM_LIKES":
+      return { ...state, likes: action.payload };
+
+    default:
+      return state;
+  }
+};
