946fd6c79145a3cf1dba2fca51cba31129c7974e
anamkaa <75376715+anamkaa@users.noreply.github.com>
2022-07-29 23:12:40 +0530

    added history functionality

--- src/pages/HistoryPage.jsx
+++ src/pages/HistoryPage.jsx
@@ -1,16 +1,47 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Aside from "../components/Aside";
+import { useHistory } from "../context/history-context";
+import DisplayVideoCard from "../components/DisplayVideoCard";
+import { useAuth } from "../context/auth-context";
+import { deleteAllHistoryService } from "../services/history-services";
 
 const PlaylistPage = () => {
+  const {
+    user: { isLogged, tokenVal },
+  } = useAuth();
+  const {
+    historyState: { history },
+    historyDispatch,
+  } = useHistory();
+
+  const deleteAllHistory = () => {
+    if (isLogged) {
+      deleteAllHistoryService(tokenVal, historyDispatch);
+    }
+  };
   return (
     <>
       <Navbar />
 
       <div className="ff-container">
         <Aside />
-
-        <div className="ff-container-content">History will Come here.</div>
+        <div className="ff-container-content">
+          <div
+            className="ff-category-text btn btn-warning btn-warning-hover ff-btn-secondary"
+            onClick={() => {
+              deleteAllHistory();
+            }}
+            style={{ maxWidth: "90px" }}
+          >
+            Clear
+          </div>
+          {history.length
+            ? history.map((item) => {
+                return <DisplayVideoCard video={item} key={item._id} />;
+              })
+            : "No History"}
+        </div>
       </div>
     </>
   );
